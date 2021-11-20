18e175943f7253c5aeda1194f37496636854774f
Zakiullah Khan <45316278+prolinkz@users.noreply.github.com>
2021-11-21 02:48:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
 
-// "use strict";
+"use strict";
 
 
 
-alert( 1 / 0 ); // Infinity
+// alert( 1 / 0 ); // Infinity
 
-let name = 'zaki';
-    alert(name);
+// let name = 'zaki';
+//     alert(name);
 
-let name = 'zaki';
-    alert(`My name is ${name}!`);
+// let name = 'zaki';
+//     alert(`My name is ${name}!`);
 
-const age = 35;
-alert(`and I am ${age} years old`);
+// const age = 35;
+// alert(`and I am ${age} years old`);
 
-let myName = prompt("What's your name?");
-console.log(`Good Morning, ${myName}`);
+// let myName = prompt("What's your name?");
+// console.log(`Good Morning, ${myName}`);
 
-let age = prompt("Enter your Date of Birth", '' );
-confirm(`Your age is ${age} - [current date]`);
+// let age = prompt("Enter your Date of Birth", '' );
+// confirm(`Your age is ${age} - [current date]`);
 
 
-let age = prompt("Enter your Date of Birth", '' );
-let date = prompt("Enter Today Date, to calculate age", '');
-alert(`${date}, - , ${age}`);
+// let age = prompt("Enter your Date of Birth", '' );
+// let date = prompt("Enter Today Date, to calculate age", '');
+// alert(`${date}, - , ${age}`);
 
 // Find your AGE
 let age = prompt("Enter Your Birth Year", "");
