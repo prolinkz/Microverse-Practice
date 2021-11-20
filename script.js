d14cd1f4a646d9ac3349b8f12d6c84865d5c1d7f
Zakiullah Khan <45316278+prolinkz@users.noreply.github.com>
2021-11-21 03:29:42 +0500

    Age Restriction using if/else statement
    Age Restriction handling using 3 functions with if/else statement

--- script.js
+++ script.js
@@ -49,6 +49,14 @@ alert(`Confirmed, your age is ${conf}`);
 
 
 
+let conf = confirm("You must be at least 18 years old to view this content.");
+let name = prompt("What's your name?","");
+let age = prompt("What's your age?","");
 
-
+if (age>=18) {
+alert("Welcome!you are eligible");
+}
+else {
+alert("Sorry, you must be at least 18 years old to view this content.");
+}
 
