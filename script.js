27aa99f9da0af3c8d0b6b5cdc4e663067e77b1fc
Zakiullah Khan <45316278+prolinkz@users.noreply.github.com>
2021-11-21 03:50:37 +0500

    update Age Restriction method script.js
    well-organized structure for Age restriction handling methods

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ alert(`Your age is ${2021 - age}`);
 
 // Find DATE OF BIRTH
 let dob = prompt("How old are you", 'in years' );
-alert(`Your date of birth is ${dob - 2021}`);
+alert(`Your date of birth is ${2021 - dob}`);
 
 //All 3 couple of Action functions to interact with the user: alert, prompt and confirm.
 let age = prompt("how old are you?",'');
@@ -60,3 +60,17 @@ else {
 alert("Sorry, you must be at least 18 years old to view this content.");
 }
 
+
+
+// // AGE Restriction using fully described in each case using if/else statement
+let conf = confirm("You must be at least 18 years old to view this content.");
+let name = prompt("What's your name?",'');
+let dob = prompt("Enter your Birth year",'');
+let age = (`${2021 - dob}`);
+
+if (age>=18) {
+alert(`Welcome! ${name} you are eligible while your age is ${age}. We wish you best of LUCK.`);
+}
+else {
+alert(`Sorry ${name} your age is ${age} and you must be at least 18 years old to view this content.`);
+}
