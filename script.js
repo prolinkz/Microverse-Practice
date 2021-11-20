ae4aa3df031142985b0a7c53fdf6bde3f04794e9
Zakiullah Khan <45316278+prolinkz@users.noreply.github.com>
2021-11-21 02:45:02 +0500

    java practice
    java practice using online tools like https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-1
     for Java Script learning tutorial https://javascript.info/alert-prompt-confirm and Online practicing Tool for create online projects https://plnkr.co/edit/?preview

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-let myName = prompt("What's your name?");
-console.log(`Good Morning, ${myName}`);
+
+
+// "use strict";
+
+
+
+// alert( 1 / 0 ); // Infinity
+
+// let name = 'zaki';
+//     alert(name);
+
+// let name = 'zaki';
+//     alert(`My name is ${name}!`);
+
+// const age = 35;
+// alert(`and I am ${age} years old`);
+
+// let myName = prompt("What's your name?");
+// console.log(`Good Morning, ${myName}`);
+
+// let age = prompt("Enter your Date of Birth", '' );
+// confirm(`Your age is ${age} - [current date]`);
+
+
+// let age = prompt("Enter your Date of Birth", '' );
+// let date = prompt("Enter Today Date, to calculate age", '');
+// alert(`${date}, - , ${age}`);
+
+// Find your AGE
+let age = prompt("Enter Your Birth Year", "");
+alert(`Your age is ${2021 - age}`);
+
+// Find DATE OF BIRTH
+let dob = prompt("How old are you", 'in years' );
+alert(`Your date of birth is ${dob - 2021}`);
